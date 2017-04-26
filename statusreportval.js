940828a56c520f62a9b117df62c76dfd7dc57103
Arpan Tolat <Arpan Tolat>
2017-04-26 10:42:00 -0500

    Commented/Cleaned

--- statusreportval.js
+++ statusreportval.js
@@ -1,5 +1,5 @@
 /*
-This Script has been composed in an object oriented pattern. There are easier workarounds to achieve the same results (simple monolithic field validations using only if-else clause blocks, without any objects). 
+This Script has been composed in an object oriented pattern, to maximize reusability. There are easier workarounds to achieve the same results (simple monolithic field validations using only if-else clause blocks, without any objects). 
 */
 
 
